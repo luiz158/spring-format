9319c73bc70e1181a35655d89b2d0fba291381e4
Phillip Webb <pwebb@pivotal.io>
2016-10-27 17:57:26 -0700

    Hacking

--- spring-format/src/main/java/io/spring/format/formatter/Formatter.java
+++ spring-format/src/main/java/io/spring/format/formatter/Formatter.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2012-2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.spring.format.formatter;
 
 import java.io.IOException;
@@ -5,6 +21,7 @@ import java.io.InputStream;
 import java.util.Map;
 import java.util.Properties;
 
+import io.spring.formatter.eclipse.formatter.DefaultCodeFormatter;
 import org.eclipse.jdt.core.formatter.CodeFormatter;
 import org.eclipse.jface.text.BadLocationException;
 import org.eclipse.jface.text.Document;
@@ -13,31 +30,41 @@ import org.eclipse.jface.text.IRegion;
 import org.eclipse.text.edits.MalformedTreeException;
 import org.eclipse.text.edits.TextEdit;
 
-import io.spring.formatter.eclipse.formatter.DefaultCodeFormatter;
-
+/**
+ * TODO.
+ *
+ * @author Phillip Webb
+ */
 public class Formatter extends CodeFormatter {
 
 	private CodeFormatter delegate = new DelegateCodeFormatter();
 
 	public String format(String source) {
 		IDocument document = new Document(source);
-		TextEdit textEdit = format(CodeFormatter.K_COMPILATION_UNIT | CodeFormatter.F_INCLUDE_COMMENTS, source, 0,
-				source.length(), 0, null);
+		TextEdit textEdit = format(
+				CodeFormatter.K_COMPILATION_UNIT | CodeFormatter.F_INCLUDE_COMMENTS,
+				source, 0, source.length(), 0, null);
 		try {
 			textEdit.apply(document);
-		} catch (MalformedTreeException | BadLocationException ex) {
+		}
+		catch (MalformedTreeException | BadLocationException ex) {
 			throw new IllegalStateException(ex);
 		}
 		return document.get();
 	}
 
-	public TextEdit format(int kind, String source, int offset, int length, int indentationLevel,
-			String lineSeparator) {
-		return delegate.format(kind, source, offset, length, indentationLevel, lineSeparator);
+	@Override
+	public TextEdit format(int kind, String source, int offset, int length,
+			int indentationLevel, String lineSeparator) {
+		return this.delegate.format(kind, source, offset, length, indentationLevel,
+				lineSeparator);
 	}
 
-	public TextEdit format(int kind, String source, IRegion[] regions, int indentationLevel, String lineSeparator) {
-		return delegate.format(kind, source, regions, indentationLevel, lineSeparator);
+	@Override
+	public TextEdit format(int kind, String source, IRegion[] regions,
+			int indentationLevel, String lineSeparator) {
+		return this.delegate.format(kind, source, regions, indentationLevel,
+				lineSeparator);
 	}
 
 	private static class DelegateCodeFormatter extends DefaultCodeFormatter {
@@ -50,14 +77,17 @@ public class Formatter extends CodeFormatter {
 		private static Map<String, String> loadOptions() {
 			try {
 				Properties properties = new Properties();
-				InputStream inputStream = Formatter.class.getResourceAsStream("formatter.prefs");
+				InputStream inputStream = Formatter.class
+						.getResourceAsStream("formatter.prefs");
 				try {
 					properties.load(inputStream);
 					return (Map) properties;
-				} finally {
+				}
+				finally {
 					inputStream.close();
 				}
-			} catch (IOException ex) {
+			}
+			catch (IOException ex) {
 				throw new IllegalStateException(ex);
 			}
 		}
